1a56e91893e3486126ac3b94a30745e1f7093377
Tom Trahan <tom@shippable.com>
2016-06-29 13:30:26 -0700

    add latest tag

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 if [ "$IS_PULL_REQUEST" != true ]; then
-  sudo docker build -t $IMAGE_NAME:$BRANCH.latest .
+  sudo docker build -t $IMAGE_NAME:$BRANCH.$BUILD_NUMBER .
+  sudo docker tag $IMAGE_NAME:$BRANCH.$BUILD_NUMBER $IMAGE_NAME:$BRANCH.latest
 else
   echo "skipping because it's a PR"
 fi
